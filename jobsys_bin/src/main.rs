02f9f89a46403e012a8eca8340a84fb069767e3f
David Pitoniak <davidpitoniak32@gmail.com>
2023-02-04 11:10:28 -0500

    feat: test new inquire library for user input
    Testing out a beautiful inquisition library for getting input from the
    user.

--- jobsys_bin/src/main.rs
+++ jobsys_bin/src/main.rs
@@ -2,6 +2,7 @@ use std::process;
 
 use anyhow::Result;
 use clap::Parser;
+use inquire::{validator::Validation, Text};
 use log::{debug, error};
 
 use jobsys_lib::system::{Cli, JobSys};
@@ -13,19 +14,38 @@ fn main() -> Result<()> {
         .filter_level(cli.verbose.log_level_filter())
         .init();
 
-    let mut jobsys = JobSys::new("".to_owned(), cli);
+    let validator = |input: &str| -> Result<Validation, _> {
+        if input.chars().count() > 5 {
+            Ok(Validation::Invalid(
+                "You're only allowed 5 characters.".into(),
+            ))
+        } else {
+            Ok(Validation::Valid)
+        }
+    };
 
-    match jobsys.run() {
-        Ok(_) => {
-            debug!("Completed Ok, Saving Data...");
-            jobsys.data_save();
-        }
-        Err(err) => {
-            error!("System Encountered an Error, Saving Data... msg: {}", err);
-            jobsys.data_save();
-            process::exit(1);
-        }
+    let status = Text::new("What are you thinking about?")
+        .with_validator(validator)
+        .prompt();
+
+    match status {
+        Ok(status) => println!("Your status is being published..."),
+        Err(err) => println!("Error while publishing your status: {}", err),
     }
+
+    // let mut jobsys = JobSys::new("".to_owned(), cli);
+    //
+    // match jobsys.run() {
+    //     Ok(_) => {
+    //         debug!("Completed Ok, Saving Data...");
+    //         jobsys.data_save();
+    //     }
+    //     Err(err) => {
+    //         error!("System Encountered an Error, Saving Data... msg: {}", err);
+    //         jobsys.data_save();
+    //         process::exit(1);
+    //     }
+    // }
 
     Ok(())
 }
